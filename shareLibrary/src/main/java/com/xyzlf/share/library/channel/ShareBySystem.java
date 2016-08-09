b0bcafdbcce7a217bd6c22d04e626688b2d50e8c
zhanglifeng <zhanglifeng@nicaifu.com>
2016-08-09 15:02:48 +0800

    解决系统分享 更多 出现 始终 仅此一次 问题

--- shareLibrary/src/main/java/com/xyzlf/share/library/channel/ShareBySystem.java
+++ shareLibrary/src/main/java/com/xyzlf/share/library/channel/ShareBySystem.java
@@ -37,7 +37,8 @@ public class ShareBySystem extends ShareBase {
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.putExtra(Intent.EXTRA_TEXT, content);
         shareIntent.setType("text/plain");
-        if(ShareUtil.startActivity(context, shareIntent)) {
+        if(ShareUtil.startActivity(context, Intent.createChooser(
+                shareIntent, context.getString(R.string.share_to)))) {
             if (null != listener) {
                 listener.onShare(ShareConstant.SHARE_CHANNEL_SYSTEM, ShareConstant.SHARE_STATUS_COMPLETE);
             }
